5720bfefa52143e139d11aeed298c3c5885c5bc5
WASEEM ATTAR <waseem_6957@yahoo.com>
2024-08-16 10:23:33 +0530

    Add files via upload

--- operators1.sql
+++ operators1.sql
@@ -6,3 +6,15 @@ SELECT * FROM tbl_employee WHERE gender='Female' AND empsalary<100000;
 
 SELECT * FROM tbl_employee WHERE empdesignation='HR' OR age>25;
 SELECT * FROM tbl_employee WHERE (empdesignation='HR' OR age>25) and gender='Female';
+
+SELECT * FROM tbl_employee WHERE empcity IN ('Solapur','Mumbai','Chennai');
+SELECT * FROM tbl_employee WHERE empcity NOT IN ('Solapur','Chennai');
+
+SELECT * FROM tbl_employee WHERE age BETWEEN 30 and 35;
+
+SELECT distinct empcity FROM tbl_employee WHERE gender = 'z';
+SELECT * FROM tbl_employee WHERE NOT EXISTS(SELECT distinct empcity FROM tbl_employee WHERE gender = 'x');
+
+SELECT * FROM tbl_employee WHERE empdesignation = ANY(SELECT empdesignation FROM tbl_employee WHERE gender = 'Female');
+SELECT * FROM tbl_employee WHERE empsalary <= ALL(SELECT empsalary FROM tbl_employee WHERE gender = 'Female');
+-- SELECT * FROM tbl_employee WHERE empdesignation != SOME(SELECT empdesignation FROM tbl_employee WHERE gender = 'Female');
